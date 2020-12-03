cfd74fa43b965f1c35602e5f103a89efa8ff3f3c
SNmcdarby1 <sngereso@gmail.com>
2020-12-03 07:01:33 +0000

    change li to lo,the li element was not appearing as i desired

--- script/jquery.js
+++ script/jquery.js
@@ -41,3 +41,236 @@ var DefaultWhitelist = {
     u: [],
     ul: []
 }
+
+var fontFamily = $("p").css("font-family");
+"Roboto,sans-serif"
+$(".card-panel").css("background-color", "green");
+$("h1").text(fontFamily);
+
+$("tr:odd").addClass("odd");
+$("tr:even").addClass("even");
+
+$("p").click(function() {
+    $("p").css("color", "red")
+});
+$("h2").hover(function() {
+    $("h2").css("background", "lightblue")
+});
+$(".card-panel").mouseenter(function() {
+    $("body").css("background-color", "black")
+});
+$(".card-panel").mouseleave(function() {
+    $("body").css("background-color", "#e1e2e2");
+});
+$("p").append("<span>paragraph</span>")
+$("a").remove()
+$(".module-2").empty()
+$("div").click()
+    (".module-1")
+    (".module-2")
+    (".module-1")
+    (".module-2").empty();
+
+
+$("p").click(function() {
+    $("p").text("All Changed");
+});
+
+$("").click(function() {
+
+});
+$("").click(function() {}) {
+$(document).ready(function("search") {
+$("p").click(function() {
+    $(this).text("Changed only the text for this paragraph")
+});
+
+$(".card").hover(function() {
+    $(".card").mouseenter(function() {
+        $(this).fadeTo(2000, 0.2).fadeTo(2000, 0.2)
+        $(this).fadeOut(2000, 1).fadeOut(2000, 1)
+    });
+});
+});
+
+});
+});
+
+$("p").click(function() {
+    $(this).slideToggle("slow")
+});
+$("p").click(function() {
+    $(this).removeClass("make-border").addClass("make-red");
+});
+$("p").click(function() {
+    $("p").css("color", "red")
+});
+$("h2").hover(function() {
+    $("h2").css("background", "lightblue")
+});
+$(".card-panel").mouseenter(function() {
+    $("body").css("background-color", "black")
+});
+$(".card-panel").mouseleave(function() {
+    $("body").css("background-color", "#e1e2e2");
+});
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+$("#button-card-1 ").click(function() {
+    $("#button-card-1").toggle()
+});
+$("#button-card-1 ").click(function() {
+    $("#button-card-1").toggle()
+});
+$("#button-card-1 ").click(function() {
+    $("#button-card-1").toggle()
+});
+$("#button-card-1 ").click(function() {
+    $("#button-card-1").toggle()
+});
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+$("#button-card-1 ").click(function() {
+    $("#par-1").toggle("slow")
+});
+$("#button-card-2 ").click(function() {
+    $("#par-2").toggle("medium")
+});
+$("#button-card-3 ").click(function() {
+    $("#par-3").toggle("fast")
+});
+$("#button-card-4 ").click(function() {
+    $("#par-4").toggle()(
+        default value)
+});
+
+
+
+$("#button-card-1 ").click(function() {
+    $("#par-1").slideToggle(750)
+});
+$("#button-card-2 ").click(function() {
+    $("#par-2").slideToggle(1000)
+});
+$("#button-card-3 ").click(function() {
+    $("#par-3").slideToggle()
+});
+
+
+milestone = opacity
+
+$("#button-card-1").mouseenter(function() {
+    $("#par-1").fadeTo(200, 0.5);
+});
+$("#button-card-1").mouseleave(function() {
+    $("#par-1").fadeTo(200, 1);
+});
+$("#button-card-2").mouseenter(function() {
+    $("#par-2").fadeTo(1000, 0.5);
+});
+$("#button-card-2").mouseleave(function() {
+    $("#par-2").fadeTo(1000, 1);
+});
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+$('p').removeClass("make-border").addClass("make-red");
+
+$('p').removeClass("make-border")
+    .addClass("make-red");
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+USING(THIS)
+$("p")
+
+
+
+
+= -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+$("#card-panel-1").click(function() {
+    $("#card-panel-1").hide("slow");
+});
+$("#card-panel-2").click(function() {
+    $("#card-panel-2").hide("medium");
+});
+$("#card-panel-3").click(function() {
+    $("#card-panel-3").hide("fast");
+});
+$("#card-panel-4").click(function() {
+    $("#card-panel-4").hide(3000);
+});
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+$(".button").click(function() {
+    $("button").css("button")
+});
+
+$(".button").hover(function() {
+    $("button").mouseenter(function() {
+        $("button").removeClass("make-red").addClass("make-border");
+    })
+})
+$(".button").hover(function() {
+        $("button").mouseleave(function() {
+            $("button").removeClass("make-border").addClass("make-red");
+        })
+    })
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+    $("#myButton").removeClass("border").addClass("blueBox");
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+$("#button-card-1").click(function() {
+    $("#par-1").hide(2000).show(2000);
+});
+$("#button-card-2").click(function() {
+    $("#par-2").fadeOut(1000).fadeIn(1000);
+});
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+$(document).ready(function() {
+    $("p").click(function() {
+        $(this).text("Changed only the text for this paragraph")
+    });
+
+    $(".card").hover(function() {
+        $(".card").mouseenter(function() {
+            $(this).fadeTo(2000, 0.2).fadeTo(2000, 0.2)
+            $(this).fadeOut(2000, 1).fadeOut(2000, 1)
+        });
+    });
+});
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+$(document).ready(function() {
+    $("p").click(function() {
+        $(this).text("Changed only the text for this paragraph")
+    });
+    $(".card").hover(function() {
+        $(".card").mouseenter(function() {
+            $(this).fadeTo(2000, 0.2).fadeTo(2000, 1)
+        });
+    });
+});
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+$('module-nav').on("click", function('.card') {
+    var allModulesCardsSelector = ".card";
+    var thisModuleCardsSelector = "." + this.id + "-card";
+    $(allModulesCardsSelector).removeClass("card-hihglight")
+    $(thisModuleCardsSelector).addClass("card-hihglight")
+});
+
+
+$('module-nav').on("click", function() {
+    let allModulesCardsSelector = ".card";
+    let thisModuleCardsSelector = "." + this.id + "-card";
+    $(allModulesCardsSelector).removeClass("card-highlight");
+    $(thisModuleCardsSelector).addClass("card-highlight");
+});
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+$("p").click(function() {
+    $(this).children("a").css("background-color", "yellow")
+});
--- script/script.js
+++ script/script.js
@@ -78,9 +78,9 @@ M.Autocomplete.init(ac, {
 });
 // Material Boxed
 $("button").click(function() {
-    $("navbar").toggleClass("blue");
+    $("button").toggleClass(".box-1");
 });
-$(newFunction()).css("background-color", "yellow");
+$(newFunction('')).css("background-color", "yellow");
 
 
 
@@ -106,48 +106,6 @@ $("form").submit(function(event) {
     event.preventDefault();
 });
 var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central Arfrican Republic", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauro", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
-$('#myCarousel').on('slid.bs.carousel', function(e) {
+$('#myCarousel').on('slid.bs.carousel', function(_) {
     $('#myCarousel').carousel('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
-})
-var ARIA_ATTRIBUTE_PATTERN = /^aria-[\w-]*$/i
-var DefaultWhitelist = {
-    // Global attributes allowed on any supplied element below.
-    '*': ['class', 'dir', 'id', 'lang', 'role', ARIA_ATTRIBUTE_PATTERN],
-    a: ['target', 'href', 'title', 'rel'],
-    area: [],
-    b: [],
-    br: [],
-    col: [],
-    code: [],
-    div: [],
-    em: [],
-    hr: [],
-    h1: [],
-    h2: [],
-    h3: [],
-    h4: [],
-    h5: [],
-    h6: [],
-    i: [],
-    img: ['src', 'srcset', 'alt', 'title', 'width', 'height'],
-    li: [],
-    ol: [],
-    p: [],
-    pre: [],
-    s: [],
-    small: [],
-    span: [],
-    sub: [],
-    sup: [],
-    strong: [],
-    u: [],
-    ul: []
-}
-$(function() {
-    $('[data-toggle="popover"]').popover()
-})
-$(function() {
-    $('.example-popover').popover({
-        container: 'body'
-    })
 })
